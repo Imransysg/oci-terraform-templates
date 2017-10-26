79d7bc3543f68f28b77d5c00f8f5c0a34dab90e2
ooha-m <omanduri@sysgain.com>
2017-10-26 11:14:47 +0530

    Update chef-workstation.tf

--- chef-automate/chef-workstation.tf
+++ chef-automate/chef-workstation.tf
@@ -5,13 +5,11 @@ resource "oci_core_instance" "chefworkstation" {
     image = "${lookup(data.oci_core_images.WLImageOCID.images[0], "id")}"
     shape = "${var.InstanceShape}"
     subnet_id = "${oci_core_subnet.chefsubnet1.id}"
-
 create_vnic_details {
     subnet_id = "${oci_core_subnet.chefsubnet1.id}"
     display_name = "${var.nicName}"
     assign_public_ip = true
-    hostname_label = "winmachine"
-  
+    hostname_label = "winmachine" 
 }
 metadata {}
 }
