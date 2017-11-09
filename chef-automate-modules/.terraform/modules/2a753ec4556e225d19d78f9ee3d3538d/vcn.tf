cbc067be72c64380afb9cda4f5e21f577b91400a
unknown <omanduri@sysgain.com>
2017-11-09 14:25:22 +0530

    update

--- chef-automate-modules/.terraform/modules/2a753ec4556e225d19d78f9ee3d3538d/vcn.tf
+++ chef-automate-modules/.terraform/modules/2a753ec4556e225d19d78f9ee3d3538d/vcn.tf
@@ -1,6 +1,6 @@
 resource "oci_core_virtual_network" "vcn" {
   cidr_block = "10.0.0.0/16"
-  dns_label = "${var.prefix}-dns"
+  dns_label = "${var.prefix}dns"
   compartment_id = "${var.compartment_ocid}"
   display_name = "${var.prefix}-vcn"
 }
