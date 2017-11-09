daff2e0f5db834395122b7472f50668451a205c2
unknown <omanduri@sysgain.com>
2017-11-09 15:49:05 +0530

    update

--- chef-automate-modules/.terraform/modules/741fe1b6bb4300f354e9be68d8209e2a/internet-gateway.tf
+++ chef-automate-modules/.terraform/modules/741fe1b6bb4300f354e9be68d8209e2a/internet-gateway.tf
@@ -1,5 +1,5 @@
 resource "oci_core_internet_gateway" "chefIG" {
     compartment_id = "${var.compartment_ocid}"
-    display_name = "${var.prefix}-igw${random_id.unq.hex}"
+    display_name = "${var.prefix}-igw"
     vcn_id = "${var.vcn_ocid}"
 }
