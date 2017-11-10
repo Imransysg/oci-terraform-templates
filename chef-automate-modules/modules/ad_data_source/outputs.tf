0b6bb589ff3e92c3508fffcd4effd8bc7c6eaa0a
unknown <omanduri@sysgain.com>
2017-11-10 12:49:23 +0530

    up

--- chef-automate-modules/modules/ad_data_source/outputs.tf
+++ chef-automate-modules/modules/ad_data_source/outputs.tf
@@ -1,9 +1,3 @@
 output "ad1_name" {
-    value = "${lookup(data.oci_identity_availability_domains.chefAD.availability_domains[0],"name")}"
-}
-output "ad2_name" {
     value = "${lookup(data.oci_identity_availability_domains.chefAD.availability_domains[1],"name")}"
-}
-output "ad3_name" {
-    value = "${lookup(data.oci_identity_availability_domains.chefAD.availability_domains[2],"name")}"
 }
