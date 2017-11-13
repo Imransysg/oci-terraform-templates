27082dbdbbbdbd92be109b343dfcbb3e94f2e5ae
sudheermareddy <sudheerm@sysgain.com>
2017-11-13 11:40:07 +0530

    Update variables.tf

--- chef-automate-modules/variables.tf
+++ chef-automate-modules/variables.tf
@@ -26,7 +26,7 @@ variable "BootStrapFile1" {
   default ="./user-data/enablepassword.sh"
 }
 variable "sshKey" {
-  default="ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAoI3G2NvBStynJXG6cXPuT8PlzJVVHUd8v7mAbK2E+d4kdm0pt/1VMWfA5p0UMaLw6vG0ZxEqUoDObHHV18cTTDG95CXXDgiM5mMPepXgeQZpG+TxJ5NJq+Z4IwPt0q6W0L9AYFCg50b7BhO3S/jngYY74kh4T63gNJ/OlUzL8hnOT93Cq+XNBrAZZdzC/k2nqLxJfM/l5EccYGnaGkdm34C4JCacRKTb43CDK9JXe3n7SrahsfeybYWsxfw69gwNj0fxwzPUDeyEsprjqZEjGGR3H58gmFe3+2TN7Bmv6TZK2H3W4EU0yQpWoPpYz0oDmyHHbf3/Ia8Vhgqg0rV3UQ== rsa-key-20171110"
+  default="ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAgjlDRLYd04RyvpSosOwsv3eGKuRQ9qTYcVnX8HOAggoxL0pNzvSPL3D8esqedIQlkkwYezFqpdCU/U0lIpEYuA6vuWcDPY5t+Z+uo+TOctDJDJHmXEvv98ezgXB18LPYgSGQH+grGlLYwZUk+kNqgy5lDZkjcLLLldRdrekcxudVCt8bOzcozOi7bGVQ77dNC5TBcf2RLmDPV+fJENZnt/q3wmomoPiQ4D4pW+ep7U1xiUF6yL23Gfcig0g9N9wOroEDMVZIyLFxL15gNqw3Du8x36yDOV+A/2NNW0/3jc+C/2YHcHt39c+ME0YvtC3GpZTHhO8ZoSd4oyCJPqAiYw== rsa-key-20171012"
 }
 variable "ssh_private_key" {
   default ="./user-data/ssh_private_key.pem"
